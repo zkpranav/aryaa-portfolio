83740951e6f0f1b7ab9368d45693dab054b57f87
zkpranav <zkpranav@icloud.com>
2021-08-29 13:54:10 +0530

    Separating sass and css files

---
+++ src/js/typewriter.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function typewriter() {
+    const element = document.querySelector('.typewriter')
+    const toRotate = element.getAttribute('data-sentences')
+    // console.log(toRotate)
+    
+    
+})
