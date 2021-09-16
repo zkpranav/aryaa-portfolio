fdd9aadea6c80934fc450973b4f05456e39f19a7
zkpranav <zkpranav@icloud.com>
2021-09-16 21:43:24 +0530

    Fixing menu issue

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,16 +4,11 @@ const menuTrigger = document.querySelector('.menu-trigger')
 const menu = document.querySelector('.menu')
 const menuCollapse = document.querySelector('.menu-collapse')
 
-gsap.set(menu, {
-    y: - (menu.clientHeight),
-    opacity: 0,
-    display: 'block'
-})
-
 const tl = gsap.timeline()
 
 menuTrigger.addEventListener('click', function handleMenuTriggerClick() {
     tl.to(menu, {
+        display: 'block',
         y: 0,
         opacity: 1,
         duration: 0.25,
@@ -25,6 +20,7 @@ menuCollapse.addEventListener('click', function handleMenuCollapseClick() {
     tl.to(menu, {
         y: - (menu.clientHeight + 5),
         opacity: 0,
+        display: 'none',
         duration: 0.25,
         ease: 'Power2.easeOut'
     })
